b73bc32cf7b9510e46b5f0302e0e4604ce18f1f8
Anders Ågren Thuné <anders@aathn.org>
2022-12-15 23:47:42 +0900

    Rewrite to match style conventions of other modes

--- mcore-mode.el
+++ mcore-mode.el
@@ -1,129 +1,130 @@
-;;; mcore-mode.el
+;;; mcore-mode.el -*- lexical-binding: t -*-
+
+(require 'pcase)
+(require 'seq)
+(require 'treesit nil 'noerror)
 
 ;;;;;;;;;;;;;;;;;;
 ;; Highlighting ;;
 ;;;;;;;;;;;;;;;;;;
 
-(setq mcore-builtin-types
-      '("()"
-        "Int"
-        "Float"
-        "Bool"
-        "Char"
-        "String"
-        "Tensor"
-        ))
+;; Please keep these lists sorted
+(defvar mcore--builtin-types
+  '("()"
+    "Int"
+    "Float"
+    "Bool"
+    "Char"
+    "String"
+    "Tensor"
+    ))
 
-(setq mcore-special-types
-      '("Unknown"))
+(defvar mcore--special-types
+  '("Unknown"))
 
-;; Please keep this list sorted
-(setq mcore-base-keywords
-      '(
-        "all"
-        "case"
-        "con"
-        "else"
-        "end"
-        "external"
-        "if"
-        "in"
-        "lam"
-        "lang"
-        "let"
-        "match"
-        "recursive"
-        "sem"
-        "switch"
-        "syn"
-        "then"
-        "type"
-        "use"
-        "using"
-        "utest"
-        "with"
-        ))
+(defvar mcore--base-keywords
+  '(
+    "all"
+    "case"
+    "con"
+    "else"
+    "end"
+    "external"
+    "if"
+    "in"
+    "lam"
+    "lang"
+    "let"
+    "match"
+    "recursive"
+    "sem"
+    "switch"
+    "syn"
+    "then"
+    "type"
+    "use"
+    "using"
+    "utest"
+    "with"
+    ))
 
-(setq mcore-special-keywords
-      '(
-        "include"
-        "mexpr"
-        ))
+(defvar mcore--special-keywords
+  '(
+    "include"
+    "mexpr"
+    ))
 
-(setq mcore-extra-keywords
-      '(
-        "hole"
-        "independent"
-        "accelerate"
-        "loop"
-        ))
+(defvar mcore--extra-keywords
+  '(
+    "hole"
+    "independent"
+    "accelerate"
+    "loop"
+    ))
 
-(setq mcore-constants
-      '(
-        "false"
-        "true"
-        "()"
-        ))
+(defvar mcore--constants
+  '(
+    "false"
+    "true"
+    "()"
+    ))
 
-(setq mcore-special-constants
-      '(
-        "never"
-        ))
+(defvar mcore--special-constants
+  '(
+    "never"
+    ))
 
-(setq mcore-builtin-functions
-      '(
-        ;; More could be added
-        "error"
-        ))
+(defvar mcore--builtin-functions
+  '(
+    ;; NOTE(aathn, 2022-12-15): More builtin functions can be added here
+    "error"
+    ))
 
-(setq mcore-keywords
-      (append
-       mcore-base-keywords
-       mcore-extra-keywords))
+(defvar mcore-font-lock-keywords
+  (let* ((mcore-keywords
+          (append
+           mcore--base-keywords
+           mcore--extra-keywords))
+         (mcore-warning
+          (append
+           mcore--special-keywords
+           mcore--special-constants
+           mcore--special-types))
 
-(setq mcore-warning
-      (append
-       mcore-special-keywords
-       mcore-special-constants
-       mcore-special-types))
+         (mcore-keywords-regexp (regexp-opt mcore-keywords 'symbols))
+         (mcore-builtin-functions-regexp (regexp-opt mcore--builtin-functions 'symbols))
+         (mcore-constants-regexp (regexp-opt mcore--constants 'symbols))
+         (mcore-builtin-types-regexp (regexp-opt mcore--builtin-types 'symbols))
+         (mcore-warning-regexp (regexp-opt mcore-warning 'symbols))
 
-(setq mcore-keywords-regexp (regexp-opt mcore-keywords 'symbols))
-(setq mcore-builtin-functions-regexp (regexp-opt mcore-builtin-functions 'symbols))
-(setq mcore-constants-regexp (regexp-opt mcore-constants 'symbols))
-(setq mcore-builtin-types-regexp (regexp-opt mcore-builtin-types 'symbols))
-(setq mcore-warning-regexp (regexp-opt mcore-warning 'symbols))
+         (mcore-types-regexp "\\_<[[:upper:]][[:word:]]*\\_>"))
+    `(
+      (,mcore-keywords-regexp   . font-lock-keyword-face)
+      (,mcore-constants-regexp  . font-lock-constant-face)
+      (,mcore-builtin-types-regexp . font-lock-type-face)
+      (,mcore-builtin-functions-regexp  . font-lock-builtin-face)
+      (,mcore-types-regexp      . font-lock-type-face)
+      (,mcore-warning-regexp     . font-lock-warning-face)
+      ))
+  "List of font lock keyword specifications to use in `mcore-mode'.")
 
-(setq mcore-types-regexp "\\_<[[:upper:]][[:word:]]*\\_>")
-
-(setq mcore-font-lock-keywords
-     `(
-       (,mcore-keywords-regexp   . font-lock-keyword-face)
-       (,mcore-constants-regexp  . font-lock-constant-face)
-       (,mcore-builtin-types-regexp . font-lock-type-face)
-       (,mcore-builtin-functions-regexp  . font-lock-builtin-face)
-       (,mcore-types-regexp      . font-lock-type-face)
-       (,mcore-warning-regexp     . font-lock-warning-face)
-       )
-     )
-
-(defvar mcore-mode-syntax-table nil "Syntax table for `mcore-mode'.")
-
-(setq mcore-mode-syntax-table
-      (let ((table (make-syntax-table)))
-        ;; Inline comment "-- ..."
-        ;; Block comment "/- ... -/"
-        (modify-syntax-entry ?- ". 123" table)
-        (modify-syntax-entry ?/ ". 14cn" table)
-        (modify-syntax-entry ?\n "> " table)
-        (modify-syntax-entry ?' "\"" table)
-        table))
+(defvar mcore-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    ;; Inline comment "-- ..."
+    ;; Block comment "/- ... -/"
+    (modify-syntax-entry ?- ". 123" table)
+    (modify-syntax-entry ?/ ". 14cn" table)
+    (modify-syntax-entry ?\n "> " table)
+    (modify-syntax-entry ?' "\"" table)
+    table)
+  "Syntax table for `mcore-mode'.")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tree-sitter support ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar mcore--treesit-font-lock-settings
-  (when (fboundp 'treesit-font-lock-rules)
+  (when (featurep 'treesit)
     (treesit-font-lock-rules
      :language 'mlang
      :feature 'type
@@ -158,7 +159,7 @@
      `((
         (var_ident) @font-lock-builtin-face
         (:match
-         ,(concat "^\\(" (string-join mcore-builtin-functions "\\|") "\\)$")
+         ,(concat "^\\(" (string-join mcore--builtin-functions "\\|") "\\)$")
          @font-lock-builtin-face)
         ))
 
@@ -188,12 +189,12 @@
 
      :language 'mlang
      :feature 'keyword
-     `([,@mcore-base-keywords] @font-lock-keyword-face
-       [,@mcore-special-keywords] @font-lock-warning-face
+     `([,@mcore--base-keywords] @font-lock-keyword-face
+       [,@mcore--special-keywords] @font-lock-warning-face
        (
         (var_ident) @font-lock-keyword-face
         (:match
-         ,(concat "^\\(" (string-join mcore-extra-keywords "\\|") "\\)$")
+         ,(concat "^\\(" (string-join mcore--extra-keywords "\\|") "\\)$")
          @font-lock-keyword-face)
         ))
 
@@ -208,46 +209,36 @@
   "Tree-sitter font-lock settings for `mcore-mode'.")
 
 ;;;;;;;;;;;;;;
-;; prettify ;;
+;; Prettify ;;
 ;;;;;;;;;;;;;;
 
 (defvar mcore-prettify-symbols-alist
-  '(("lam" . 955)                      ; λ
-    ("all" . 8704)                     ; ∀
-    ("->" . 8594))                     ; →
+  '(("lam" . ?λ)
+    ("all" . ?∀)
+    ("->" . ?→))
   "List of syntax to prettify for `mcore-mode'.")
 
-(if (boundp 'prettify-symbols-alist)
-    (add-hook 'mcore-mode-hook
-              (lambda ()
-                (mapc (lambda (pair) (push pair prettify-symbols-alist))
-                      mcore-prettify-symbols-alist))))
-
 ;;;;;;;;;;;;;;;;;
-;; compilation ;;
+;; Compilation ;;
 ;;;;;;;;;;;;;;;;;
 
-(add-hook 'mcore-mode-hook
-          (lambda ()
-            ;; Set default compile command
-            (progn
-              (set (make-local-variable 'compile-command)
-                   (concat "mi " (buffer-name)))
-              ;; Get location of standard library from environment
-              (let ((path
-                     (replace-regexp-in-string
-                      "[[:space:]\n]*$" ""
-                      (shell-command-to-string "$SHELL -l -c 'echo $MCORE_STDLIB'"))))
-                (if (> (length path) 0)
-                  (set (make-local-variable 'compilation-environment)
-                       (list (concat "MCORE_STDLIB=" path))))))))
+(defun mcore--setup-compile ()
+  ;; Set default compile command
+  (set (make-local-variable 'compile-command)
+       (concat "mi " (buffer-name)))
+  ;; Get location of standard library from environment
+  (let ((path
+         (replace-regexp-in-string
+          "[[:space:]\n]*$" ""
+          (shell-command-to-string "$SHELL -l -c 'echo $MCORE_STDLIB'"))))
+    (if (> (length path) 0)
+        (set (make-local-variable 'compilation-environment)
+             (list (concat "MCORE_STDLIB=" path))))))
 
-(setq mcore-error-regexp
-      '(mcore "\"\\(.+\\)\" \\([0-9]+\\):\\([0-9]+\\)" 1 2 3))
-(add-hook 'compilation-mode-hook
-          (lambda ()
-            (add-to-list 'compilation-error-regexp-alist-alist mcore-error-regexp)
-            (add-to-list 'compilation-error-regexp-alist 'mcore)))
+(let ((mcore-error-regexp
+       '(mcore "\"\\(.+\\)\" \\([0-9]+\\):\\([0-9]+\\)" 1 2 3)))
+  (add-to-list 'compilation-error-regexp-alist-alist mcore-error-regexp)
+  (add-to-list 'compilation-error-regexp-alist 'mcore))
 
 ;;;;;;;;;;;
 ;; Imenu ;;
@@ -264,6 +255,8 @@
   (seq-filter
    #'cdr
    (list
+    ;; Make a menu Langs, with a submenu for each language fragment in the file
+    ;; containing the sems, syns and types of that fragment.
     `("Langs"
       .
       ,(mapcar
@@ -283,6 +276,8 @@
                      (syn_decl (type_ident) @match))))))))
         (treesit-query-capture
          'mlang '((mlang) @match))))
+    ;; Make a menu Types, with a submenu for each top-level type in the file
+    ;; containing the constructors of that type.
     `("Types"
       .
       ,(mapcar
@@ -303,6 +298,7 @@
                     `(,(treesit-node-text node t) . ,(treesit-node-start node))))))))
         (treesit-query-capture
          'mlang '((type_bind (type_ident) @match)))))
+    ;; Make a menu Lets containing all top-level (MLang) lets in the file.
     `("Lets"
       .
       ,(mapcar
@@ -312,21 +308,28 @@
          'mlang '((source_file (let_bind (var_ident) @match)))))))))
 
 ;;;;;;;;;;;;;;;;;;;;;
-;; mode definition ;;
+;; Mode definition ;;
 ;;;;;;;;;;;;;;;;;;;;;
 
 ;;;###autoload
-(define-derived-mode mcore-mode prog-mode "mcore"
-  "Major mode for editing MCore files."
-  (setq-local font-lock-defaults '(mcore-font-lock-keywords))
+(define-derived-mode mcore-base-mode prog-mode "mcore"
+  "Generic mode for editing MCore files."
   (setq-local comment-start "--")
   (setq-local comment-end "")
+  (setq-local prettify-symbols-alist mcore-prettify-symbols-alist)
+  (mcore--setup-compile))
+
+;;;###autoload
+(define-derived-mode mcore-mode mcore-base-mode "mcore"
+  "Major mode for editing MCore files."
+  (setq-local font-lock-defaults '(mcore-font-lock-keywords))
   (setq-local imenu-generic-expression mcore--imenu-generic-expression))
 
 ;;;###autoload
-(define-derived-mode mcore-ts-mode prog-mode "mcore"
+(define-derived-mode mcore-ts-mode mcore-base-mode "mcore"
   "Major mode for editing MCore files, powered by tree-sitter."
-  (unless (and (fboundp 'treesit-ready-p)
+  :syntax-table mcore-mode-syntax-table
+  (unless (and (featurep 'treesit)
                (treesit-ready-p 'mlang))
     (error "Tree-sitter for MLang isn't available"))
   (setq-local treesit-font-lock-settings mcore--treesit-font-lock-settings)
@@ -335,8 +338,6 @@
                 (keyword type builtin constant)
                 (function-name variable-name)
                 (pattern-name label-name)))
-  (setq-local comment-start "--")
-  (setq-local comment-end "")
   (setq-local imenu-create-index-function #'mcore--treesit-imenu-index-function)
   (treesit-major-mode-setup))
 
